0032382de16d094c8b7e3872c054b99a80a98409
BALAJI BEDRE <89333765+Balajibedre@users.noreply.github.com>
2021-10-22 20:04:17 +0530

    Add files via upload

---
+++ company.py
@@ -0,0 +1,14 @@
+class Employee:
+    company = 'Google'
+    salary = 34
+
+pragati = Employee()
+akshay = Employee()
+# print(pragati.company)    
+# Employee.company = 'YouTube'
+pragati.salary = 25
+akshay.salary = 20
+# akshay.company = 'wns'
+print(pragati.salary)    
+print(akshay.salary)    
+print(akshay.company)
