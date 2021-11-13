37e3db32272542dd84eb55fc5c1103be1f07f139
BALAJI BEDRE <89333765+Balajibedre@users.noreply.github.com>
2021-11-13 12:11:42 +0530

    Add files via upload
    add

---
+++ code16.py
@@ -0,0 +1,39 @@
+basics of html
+
+<html></html>   start and end document .
+
+<h1></h1>     hedder sizes 1-6 (larger to small)
+
+<p></p>     paragraph tag 
+
+&nbsp      non - breaking space .
+
+<b></b>    bold text tag .
+
+<strike></strike>  strike through tag 
+
+<i></i>    italics tag 
+
+<li></li>  list item tag 
+
+<table></table>  basic table tag 
+
+<a href="url">text</html>  basic link
+
+<img src="url" alt="text"   basic image
+
+src = "url"  source(url/file name). 
+
+alt = "text"  alt(hover) text . 
+
+width = "number"  line or image width by number or percentage
+
+height = "number" line or image height by number or percentage
+
+border = "number"  border thickness 
+
+colour = "#number"  colour
+
+<br>   line break tag 
+
+<blockquote></blockquote>   quote text block
