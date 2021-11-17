1e74e146b31551adb7a83d11f0a254c5f36a5a91
BALAJI BEDRE <89333765+Balajibedre@users.noreply.github.com>
2021-11-17 11:56:59 +0530

    Update code17.py

--- code17.py
+++ code17.py
@@ -5,3 +5,18 @@ while i < 6:
   if i == 3:
     break
   i += 1
+
+python inheritances
+
+  class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+
+
+x = Person("John", "Doe")
+x.printname()
