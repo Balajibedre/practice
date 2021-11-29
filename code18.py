355d9b950873d313f7af070b7fd75c2a70ecba72
BALAJI BEDRE <89333765+Balajibedre@users.noreply.github.com>
2021-11-29 18:22:29 +0530

    Update code18.py

--- code18.py
+++ code18.py
@@ -8,3 +8,14 @@ def Pattern(n):
         print(\r)
 n =5
 pattern(n)
+
+def Pattern(n):
+    num = 100
+    for i in range(o,n):
+    for j in range(0,i+1):
+        ch = chr(num)
+        print(ch,end="")
+        num = num+1
+        print(\r)
+n = 5
+pattern(n)
