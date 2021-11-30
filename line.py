f7a34e2748621ba88181b7c6839f8f45594ee892
BALAJI BEDRE <89333765+Balajibedre@users.noreply.github.com>
2021-11-30 11:49:17 +0530

    Update line.py

--- line.py
+++ line.py
@@ -7,3 +7,13 @@
             
 #             print(f"yes is present in line number{i}")
 #         i+=1    
+
+content = False
+i = 1
+with open('another.txt') as f:
+            while content:
+                        content = f.readline()
+                        if 'yes' in content.lower()
+                        
+                        print(f"yes is present in line number{i}")
+                        i+=1
